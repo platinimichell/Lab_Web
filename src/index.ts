82bd74db72a375cbf0f69eea98800e0dc9d4cbdc
Junio Goncalves <junio-gon@hotmail.com>
2025-03-15 14:09:17 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import userRoutes from "./presentation/routes/userRoutes"
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use("/api", userRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Application is running on port ${PORT}`));
